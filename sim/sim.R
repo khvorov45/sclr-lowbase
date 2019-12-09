f6e676677a77a1397b04c2393a3948f9a11281ee
Arseniy Khvorov <khvorov45@gmail.com>
2019-12-10 10:05:26 +1100

    added mult per household sims

--- sim/sim.R
+++ sim/sim.R
@@ -21,18 +21,30 @@ exp_probs <- seq(0.1, 1, 0.1)
 
 # Functions ===================================================================
 
-sim_dat <- function(exposure_prob,
+sim_dat <- function(exposure_prob, n_per_hhold = 1,
                     seed = sample.int(.Machine$integer.max, 1)) {
   set.seed(seed)
   nsam <- 1e4
+  n_hhold <- nsam / n_per_hhold
   pop <- tibble(
-    logTitre = rnorm(nsam, 2, 2),
-    inf_prob = 0.5 / (1 + exp(-5 + 1.5 * logTitre)),
-    exposed = rbern(nsam, exposure_prob),
-    status = rbern(nsam, inf_prob * exposed)
-  )
+    hhold = seq(1:n_hhold),
+    exposed = rbern(n_hhold, exposure_prob)
+  ) %>%
+    slice(rep(1:n(), each = n_per_hhold)) %>%
+    mutate(
+      logTitre = rnorm(n(), 2, 2),
+      inf_prob = 0.5 / (1 + exp(-5 + 1.5 * logTitre)),
+      status = rbern(n(), inf_prob * exposed)
+    )
+  exp_hhold_meas <- pop %>%
+    group_by(hhold) %>%
+    filter(sum(status == 1) > 0) %>%
+    pull(hhold)
+  pop <- pop %>%
+    mutate(exposed_meas = hhold %in% exp_hhold_meas)
   attr(pop, "seed") <- seed
   attr(pop, "exposure_prob") <- exposure_prob
+  attr(pop, "n_per_hhold") <- n_per_hhold
   pop
 }
 
@@ -45,35 +57,52 @@ fit_sclr_one <- function(dat, pop_name) {
     mutate(
       pop = pop_name,
       exposure_prob = attr(dat, "exposure_prob"),
+      n_per_hhold = attr(dat, "n_per_hhold"),
       seed = attr(dat, "seed")
     )
 }
 
 fit_sclr <- function(dat) {
-  bind_rows(
+  always <- bind_rows(
     fit_sclr_one(dat, "general"),
     fit_sclr_one(filter(dat, exposed == 1), "exposed")
   )
+  if (attr(dat, "n_per_hhold") > 1) {
+    sometimes <- fit_sclr_one(filter(dat, exposed_meas), "exposed_meas")
+    always <- bind_rows(always, sometimes)
+  }
+  always
 }
 
-sim_fit_one <- function(exposure_prob,
+sim_fit_one <- function(exposure_prob, n_per_hhold = 1,
                         seed = sample.int(.Machine$integer.max, 1)) {
-  fit_sclr(sim_dat(exposure_prob, seed))
+  fit_sclr(sim_dat(exposure_prob, n_per_hhold, seed))
 }
 
-sim_fit_many <- function(exposure_prob, nsim) {
-  future_map_dfr(1:nsim, ~ sim_fit_one(exposure_prob))
+sim_fit_many <- function(exposure_prob, n_per_hhold = 1, nsim = 2) {
+  future_map_dfr(1:nsim, ~ sim_fit_one(exposure_prob, n_per_hhold))
 }
 
-vary_ep <- function(ep_range, nsim) {
-  map_dfr(ep_range, sim_fit_many, nsim)
+vary_ep <- function(ep_range, n_per_hhold = 1, nsim = 2) {
+  map_dfr(ep_range, sim_fit_many, n_per_hhold, nsim)
 }
 
 save_res <- function(sims) {
   write_csv(sims, file.path(sim_dir, paste0("res-", nsim, "sims.csv")))
 }
 
+add_res <- function(sims) {
+  filep <- file.path(sim_dir, paste0("res-", nsim, "sims.csv"))
+  all_sims <- bind_rows(read_csv(filep, col_types = cols()), sims)
+  write_csv(all_sims, filep)
+}
+
 # Script ======================================================================
 
-sims <- vary_ep(exp_probs, nsim)
+# One per household
+sims <- vary_ep(exp_probs, 1, nsim)
 save_res(sims)
+
+# Multiple per household
+sims_mult <- vary_ep(exp_probs, 4, nsim)
+add_res(sims_mult)
